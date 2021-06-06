a5a099ae70522c7f140f8d6a0aef313fe1fa730a
BepHuTe-CaMoKaT <55881526+BepHuTe-CaMoKaT@users.noreply.github.com>
2021-06-06 11:07:08 +0300

    Add files via upload

---
+++ task2.sql
@@ -0,0 +1,8 @@
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+
+select max(salary) as SecondHighestSalary
+from Employee where salary < (select max(salary) from employee);
